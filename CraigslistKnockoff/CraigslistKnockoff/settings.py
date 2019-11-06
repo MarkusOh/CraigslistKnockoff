dc4196d074f4f934460d3fb7d522829484285498
“MarkusOh” <markumichaeloh@gmail.com>
2019-11-06 22:40:52 +0900

    Add Frontend and requirements

--- CraigslistKnockoff/CraigslistKnockoff/settings.py
+++ CraigslistKnockoff/CraigslistKnockoff/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'my_app',
+    'my_app.apps.MyAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +119,4 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
--- CraigslistKnockoff/my_app/views.py
+++ CraigslistKnockoff/my_app/views.py
@@ -1,4 +1,13 @@
+import requests
 from django.shortcuts import render
+from bs4 import BeautifulSoup
 
 def home(request):
     return render(request, 'my_app/base.html')
+
+def new_search(request):
+    search = request.GET.get('search_text')
+    results = {
+        'search': search
+    }
+    return render(request, 'my_app/new_search.html', results)
